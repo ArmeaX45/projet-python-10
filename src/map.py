c4a33fb4dad3c5db6a6dd9d63210c289ae527fa1
LeFouMbappe <g.pasquiermenard@gmail.com>
2025-11-03 14:36:07 +0100

    add on map def add on gridd

--- src/map.py
+++ src/map.py
@@ -7,7 +7,13 @@ class map():
   def __init__(self, hauteur = 120, largeur = 120):
     self.width = largeur
     self.height = hauteur
-    self.matrice = [[ '-' for _ in range(0, self.widht) ] '-' for _ in range(0, self.height)]
+    self.matrice = [[ '-' for _ in range(0, self.widht) ]  for _ in range(0, self.height)]
+  
+  def add_on_grid(self, soldat):
+        grid_y = soldat.rect.y // soldat.rect.height
+        grid_x = soldat.rect.x // soldat.rect.width
+        
+        self.grid[grid_y][grid_x] = soldat.tag
 
   
 
