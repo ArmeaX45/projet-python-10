c97210f1e6d952078ccc72f83f64138cdfe4cd5e
Le-Fou-Mbappe <g.pasquiermenard@gmail.com>
2025-11-03 14:15:14 +0100

    Update map.py

--- src/map.py
+++ src/map.py
@@ -7,7 +7,8 @@ class map():
   def __init__(self, hauteur = 120, largeur = 120):
     self.width = largeur
     self.height = hauteur
-    self.matrice = [[ '-' in range(
+    self.matrice = [[ '-' for _ in range(0, self.widht) ] '-' for _ in range(0, self.height)]
+
   
 
 
