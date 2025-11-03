982e2105671991d431cbc6780437c23cdc5d7a4f
LeFouMbappe <g.pasquiermenard@gmail.com>
2025-11-03 14:43:14 +0100

    decaler

--- src/map.py
+++ src/map.py
@@ -4,18 +4,41 @@ import pygame
 
 
 class map():
-  def __init__(self, hauteur = 120, largeur = 120):
-    self.width = largeur
-    self.height = hauteur
-    self.matrice = [[ '-' for _ in range(0, self.widht) ]  for _ in range(0, self.height)]
-  
-  def add_on_grid(self, soldat):
+    def __init__(self):
+        self.width = 12
+        self.height = 12
+        self.grid = [['-' for _ in range(self.width)] for _ in range(self.height)]
+        
+        self.all_soldats = pygame.sprite.Group()
+        
+        
+    
+    def add_on_grid(self, soldat):
         grid_y = soldat.rect.y // soldat.rect.height
         grid_x = soldat.rect.x // soldat.rect.width
         
         self.grid[grid_y][grid_x] = soldat.tag
-def le bouufehfvbek fugiezgyerz
-  
+        
+    
+    def show_grid(self, stdscr):
+        for y, line in enumerate(self.grid):
+            for x, tag in enumerate(line):
+                stdscr.addstr(y+1, x * 2, tag) # y+1 because the first line is the title so we add an offset
+        stdscr.refresh()
+    
+    
+    def start_cmd(self, stdscr):
+        curses.curs_set(0)  # Hide the cursor
+    
+        stdscr.clear()      # Clear the screen
+        
+        self.show_grid(stdscr)
+        
+        stdscr.refresh()    # Refresh the screen
+        stdscr.getch()      # Waiting for a key press
+        
+    def add_to_soldat_group(self, soldat):
+        self.all_soldats.add(soldat)
 
 
 
