a18bfeba97e3d503380bc4916578e124ee1eb431
Benjamin-cvl <benjamin.zhang1976@gmail.com>
2025-11-07 14:29:37 +0100

    Update soldat.py

--- src/soldat.py
+++ src/soldat.py
@@ -7,9 +7,19 @@ class Soldat(pygame.sprite.Sprite):
     
     instances = []  # Shared list for each object Soldat
     
-    def __init__(self, x, y, img_path):
-        
+    def __init__(self, name, hp, attack, armor, armor_pierce, range, vision_range, speed, reload, symbol="",x, y, img_path):
         super().__init__()
+        self.name = name
+        self.hp = hp
+        self.attack = attack
+        self.armor = armor
+        self.armor_pierce = armor_pierce
+        self.range = range
+        self.vision_range = vision_range
+        self.speed = speed
+        self.reload = reload
+        self.symbol = symbol
+        
         Soldat.instances.append(self) # Adding the object to the shared list
 
         if img_path:
@@ -20,6 +30,11 @@ class Soldat(pygame.sprite.Sprite):
 
         
         self.is_alive = True     # It's true if the soldier has more than 0 HP.
+    
+    def __repr__(self):
+        return (f"Unit(name={self.name}, hp={self.hp}, attack={self.attack}, armor={self.armor}, "
+            f"armor_pierce={self.armor_pierce}, range={self.range}, vision_range={self.vision_range}, "
+            f"speed={self.speed}, reload={self.reload}, symbol={self.symbol})")
         
     def __str__(self):
         return f"The {self.name} soldat have {self.hp}HP"
