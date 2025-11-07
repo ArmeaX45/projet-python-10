fd03808a144a1709d7e06e59e22c0cf79ba034d9
Novah974-8 <n.techer@rt-iut.re>
2025-11-07 15:06:11 +0100

    Fonction Move() V1

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
 """File: main.py"""
 
-from src.game import Game
+# from src.game import Game
+from src.map import Map
 from src.halberdier import Halberdier
 from src.paladin import Paladin
 from src.arbalester import Arbalester
 
 import pygame
 import curses
+import time
 
 
 if __name__ == "__main__":
-    
-    game = Game()
+
+    map = Map()
 
     halberdier = Halberdier(0,0)
-    paladin = Paladin(11,11)
+    paladin = Paladin(10,1)
     arbalester = Arbalester(5,8)
     
     print(halberdier.instances)
     for soldat in halberdier.instances:
-        game.add_to_soldat_group(soldat)
-        game.add_on_grid(soldat)
-        
-    curses.wrapper(game.start_cmd)
+        map.add_to_soldat_group(soldat)
+        map.add_on_grid(soldat)
+    
+    curses.wrapper(map.start_cmd)
+    
+    halberdier.move(map, dx=1)
+    paladin.move(map, dy=1)
+    arbalester.move(map, dy=-1)
+    curses.wrapper(map.start_cmd)
+
 
     # print(str(halberdier))
     # print(str(paladin))
--- src/soldat.py
+++ src/soldat.py
@@ -17,12 +17,13 @@ class Soldat(pygame.sprite.Sprite):
             self.rect = self.image.get_rect()
             self.rect.x = x * self.rect.width
             self.rect.y = y * self.rect.height
-
-        
+            
         self.is_alive = True     # It's true if the soldier has more than 0 HP.
+        
         
     def __str__(self):
         return f"The {self.name} soldat have {self.hp}HP"
+    
     
     def attack(self, soldat):
         armor = soldat.armor if self.is_close_combat else soldat.armor_pierce   # Select the type of armour to use
@@ -37,8 +38,39 @@ class Soldat(pygame.sprite.Sprite):
         
         return None
     
-    def move(self, dx, dy):
-        pass
+    
+    def move(self,map, dx=None, dy=None):
+        
+        grid_x = self.rect.x // self.rect.width
+        grid_y = self.rect.y // self.rect.height
+        
+        map.grid[grid_y][grid_x] = '-'
+        if dx:
+            new_x = self.rect.x + dx * self.rect.width * self.speed
+            if 0 < new_x < map.width * self.rect.width:
+                self.rect.x += dx * self.rect.width * self.speed
+                grid_x = self.rect.x // self.rect.width
+            elif 0 < new_x:
+                self.rect.x = 0
+            elif new_x < map.width * self.rect.width:
+                self.rect.x = map.width * self.rect.width - self.rect.width
+
+        else:
+            new_y = self.rect.y + dy * self.rect.height * self.speed
+            if 0 < new_y < map.height *  self.rect.height:
+                self.rect.y = new_y
+                grid_y = self.rect.y // self.rect.height
+            elif 0 < new_y:
+                self.rect.y = 0
+            elif new_y < map.height * self.rect.height:
+                self.rect.y = map.height * self.rect.height - self.rect.height
+
+        map.grid[grid_y][grid_x] = self.tag
+        
+        
+        
+            
+        
         
         
     
